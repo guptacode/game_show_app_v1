3f67de1fd23087ab9ec6e18418b72202f776cf35
V Gupta <vivgupta@me.com>
2021-10-02 10:45:42 +0100

    Add JSDoc to functions & modify checkLetter() and keyboard event handler

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const startScreen = document.querySelector('#overlay');
 const keyboard = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase ul');
+const scoreboard = document.querySelector('#scoreboard ol');
+const allLetters = phrase.children;
+const hearts = scoreboard.children;
 let missed = 0;
 const phrases = [
     "A CAT HAS NINE LIVES",
@@ -10,12 +13,26 @@ const phrases = [
     "HANGING ON MY EVERY WORD",
 ];
 
+/**
+ * This function chooses a random phrase from the phrases array and splits it up by each individual letter
+ *
+ * @param {Array} arr - An array of strings
+ * @returns {Array} An array of the individual letters within the randomly chosen phrase
+ */
+
 function getRandomPhraseAsArray(arr) {
     const randomIndex = Math.floor( Math.random() * arr.length );
     const randomPhrase = arr[randomIndex];
     const letters = randomPhrase.split("");
     return letters;
 }
+
+/**
+ * This function takes the individual letters of a phrase and attaches them as individual list items to the DOM
+ *
+ * @param {Array} arr - An array of individual letters
+ * @returns {Object} list items that contain individual letters of the phrase
+ */
 
 function addPhraseToDisplay(arr) {
     for ( let i = 0; i < arr.length; i++) {
@@ -28,18 +45,34 @@ function addPhraseToDisplay(arr) {
     }
 }
 
+/**
+ * This function checks to see if the letter that the player chose is present in the phrase
+ *
+ * @param {Object} chosenLetter - The list item selected by the player that contains the letter
+ * @returns {Object} If the letter is matched to the phrase, it is returned with the "show" class added
+ * @returns {null} If the letter is not matched to the phrase, it returns null
+ */
+
 function checkLetter(chosenLetter) {
-    const allLetters = phrase.children;
-    for ( i = 0; i < allLetters.length; i++ ) {
+    let letterCount = 0;
+    let letterFound = [];
+    for ( let i = 0; i < allLetters.length; i++ ) {
         if ( allLetters[i].className === 'letter' && chosenLetter.textContent === allLetters[i].textContent.toLowerCase() ) {
             allLetters[i].classList.add( 'show' );
-            let letterFound = allLetters[i];
+            letterCount++;
+            letterFound.push( allLetters[i] );
+            // let letterFound = allLetters[i];
+            // return letterFound;
+            }
+        }
+        if ( letterCount === 0 ) {
+            return null;
+        } else {
             return letterFound;
         }
-        }
-        return null;
     }
 
+// Add event handler to 'Start Game' button, which will remove overlay
 
 startScreen.addEventListener ( 'click', (e) => {
     const startButton = e.target;
@@ -48,12 +81,31 @@ startScreen.addEventListener ( 'click', (e) => {
     }
 });
 
+// Add event handler to keys on keyboard, which will check players choices and accordingly either show letters or subtract lives
+
 keyboard.addEventListener ( 'click', (e) => {
     let chosenLetter = e.target;
     if ( chosenLetter.tagName === 'BUTTON') {
         chosenLetter.classList.add( 'chosen' );
         chosenLetter.setAttribute( 'disabled', '');
         let letterFound = checkLetter(chosenLetter);
+        if ( !letterFound ) {
+            missed++;
+            let li = document.createElement('li');
+            li.className = 'tries';
+            let img = document.createElement('img');
+            img.setAttribute( 'src', 'images/lostHeart.png' );
+            img.setAttribute( 'height', '35px' );
+            img.setAttribute( 'width', '30px' );
+            li.appendChild(img);
+            scoreboard.insertBefore( li, hearts[missed] );
+            scoreboard.removeChild( hearts[missed - 1] );
+            
+            // *** Alternative way of removing tries from the scoreboard *** 
+            
+            // let lifeLost = hearts[missed - 1].children;
+            // lifeLost[0].setAttribute( 'src', 'images/lostHeart.png' );
+        }
     }
 });
 
